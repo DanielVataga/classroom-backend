66321996cf509eea38a1f2e9384d95dfd679ba3f
DanielVataga <danikvataga77@gmail.com>
2026-05-06 15:51:49 +0100

    implement betterauth

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import cors from "cors";
+
 import express from "express";
 import subjectsRouter from "./routes/subjects";
-import cors from "cors";
+
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app = express();
 const PORT = 8000;
@@ -15,6 +19,8 @@ app.use(
     credentials: true,
   }),
 );
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
